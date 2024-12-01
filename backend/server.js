c83a84640607318d2ff003a92e8164b7943058ad
REZ3X <rezex.dev01@gmail.com>
2024-12-01 09:35:40 +0700

    edit_link_boyy

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 const app = express();
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Replace with your frontend URL
+  res.header('Access-Control-Allow-Origin', 'https://quotes.rezex.my.id'); // Replace with your frontend URL
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
